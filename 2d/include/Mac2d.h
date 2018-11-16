c77dfe2e41f1ab61aa1f6ea5906fff646aa164f5
Sean Bone <seanbone@zumguy.com>
2018-11-16 22:51:53 +0100

    Began work on visualization

--- 2d/include/Mac2d.h
+++ 2d/include/Mac2d.h
@@ -2,6 +2,7 @@
 #define Mac2d_h
 
 #include <Eigen/Sparse>
+#include <vector>
 
 using Triplet_t = Eigen::Triplet<double>; 
 using Pair_t = std::pair<int, int>;
@@ -17,16 +18,28 @@ class Mac2d{
 		double* ppressure_;		//pointer to array for the pressure
 		double* pu_;			//pointer to array for the velocities in x-direction
 		double* pv_;			//pointer to array for the velocities in y-direction
-		Triplet_t* pA_diag_;		//sparse matrix which contains the data for the pressure equations
 		bool* psolid_; 			//pointer to array for specifing if a cell is solid (1) or not(0)
 		
+		// diagonal of the sparse matrix A used to solve th pressure equations
+		std::vector<Triplet_t> pA_diag_;
+
 	public:
-		Mac2d() {}												//Default constructur
-		Mac2d(const int n, const int m);						//Constructor with a dimension
-		double get_u(const int i, const int j);					//Get the x-velocity in the mathematical point (i-1/2,j) 
-		double get_v(const int i, const int j);					//Get the y-velocity in the mathematical point (i,j-1/2)
-		double get_pressure(const int i, const int j);			//Get the pressure in the mathematical point(i,j)
-		bool is_solid(const int i, const int j);				//Return if the cell with center (i,j) is a solid cell
-		Pair_t index_from_coord(const double x, const double y);	//Return a pair with the grid-coordinates (i,j) given a spatial coordinate(x,y)	
+		Mac2d() {}							//Default constructur
+		Mac2d(const int n, const int m);	//Constructor with a dimension
+
+		//Get the x-velocity in the mathematical point (i-1/2,j) 
+		double get_u(const int i, const int j);
+
+		//Get the y-velocity in the mathematical point (i,j-1/2)
+		double get_v(const int i, const int j);
+
+		//Get the pressure in the mathematical point(i,j)
+		double get_pressure(const int i, const int j);
+
+		//Return if the cell with center (i,j) is a solid cell
+		bool is_solid(const int i, const int j);
+
+		//Return a pair with the grid-coordinates (i,j) given a spatial coordinate(x,y)	
+		Pair_t index_from_coord(const double x, const double y);
 };
 #endif

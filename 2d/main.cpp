014ede8762830255a9c702ed66c5f5c73769e716
Sean Bone <seanbone@zumguy.com>
2018-11-17 10:29:37 +0100

    Added simulation parameters to GUI

--- 2d/main.cpp
+++ 2d/main.cpp
@@ -9,6 +9,16 @@
  * defined in Gui.h.
  */
 class WaterGui : public Gui {
+   private:
+      bool m_export_meshes = false;
+      int m_export_fps = 30;
+
+      double m_system_size_x = 10; // X dimension of system in m
+      double m_system_size_y = 10; // Y dimension of system in m
+
+      int m_grid_res_x = 100; // Number of cells on X axis
+      int m_grid_res_y = 100; // Number of cells on Y axis
+
    public:
       WaterSim *p_waterSim = NULL;  // pointer to the simulation
 
@@ -23,9 +33,40 @@ class WaterGui : public Gui {
          start();
       }
 
-      virtual void updateSimulationParameters() override{
-         // TODO: add simulation parameters to GUI
+      /**
+       * Update the Simulation class with the parameters input to the GUI
+       */
+      virtual void updateSimulationParameters() override {
+         std::cout << "\n*********\n Simulation Parameters:\n\n";
+         std::cout << "Export meshes:\t" << m_export_meshes << std::endl;
+         std::cout << "Export freq.:\t" << m_export_fps << std::endl;
+         std::cout << "X Size [m]:\t" << m_system_size_x << std::endl;
+         std::cout << "Y Size [m]:\t" << m_system_size_y << std::endl;
+         std::cout << "Resolution X:\t" << m_grid_res_x << std::endl;
+         std::cout << "Resolution Y:\t" << m_grid_res_y << std::endl;
+         std::cout << "\n*********\n";
       };
+
+      /**
+       * Add parameter controls to the GUI
+       */
+      virtual void drawSimulationParameterMenu() override {
+//            if (ImGui::Button("Export Trajectories", ImVec2(-1, 0))) {
+//               exportTrajectories();
+//            }
+//            ImGui::SliderAngle("Angle", &m_angle, -180.0f, 180.0f);
+//            ImGui::InputFloat("Force", &m_force, 0, 0);
+//            ImGui::InputFloat("Mass", &m_mass, 0, 0);
+//            ImGui::InputFloat("dt", &m_dt, 0, 0);
+//            ImGui::Combo("Integrator", &m_selected_integrator, m_integrators.data(),
+//                         m_integrators.size());
+            ImGui::Checkbox("Export meshes", &m_export_meshes);
+            ImGui::InputInt("Export FPS", &m_export_fps, 0, 0);
+            ImGui::InputDouble("X Size [m]", &m_system_size_x, 0, 0);
+            ImGui::InputDouble("Y Size [m]", &m_system_size_y, 0, 0);
+            ImGui::InputInt("Grid resolution X", &m_grid_res_x, 0, 0);
+            ImGui::InputInt("Grid resolution Y", &m_grid_res_y, 0, 0);
+      }
 };
 
 int main() { //int argc, char *argv[]) {

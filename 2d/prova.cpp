bf9b430f1bc03eb940c034b79bb29b6a4dadabf0
Silvia Nauer <snauer@student.ethz.ch>
2018-11-18 12:03:05 +0100

    remove of two stupid files

--- 2d/prova.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/Mac2d.cpp"
-
-int main(){
-	Mac2d *grid = new Mac2d(4,3,0.1,0.1);	
-	std::cout << (grid->index_from_coord(5.6, 7.8)).first << "     " << (grid->index_from_coord(5.6, 7.8)).second << std::endl;
-	
-	return 0;
-}

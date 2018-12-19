db87402ec47e07fecf34e17c5c33c2a15db9ceb8
Silvia Nauer <snauer@student.ethz.ch>
2018-12-19 22:22:36 +0100

    Comment of MeshExporter.cpp and MeshExporter.h

--- 3d/include/MeshExporter.h
+++ 3d/include/MeshExporter.h
@@ -17,9 +17,16 @@
 
 class MeshExporter{
 	private:
+		//pointer to the MacGrid
 		Mac3d* pMacGrid_;
+		
+		//pointer to the fluid particle
 		Particle* pparticles_;
+		
+		//number of fluid particles in the list pparticles_
 		unsigned num_particles_;
+		
+		//counter for the number of exported meshes
 		unsigned num_exported_ = 0;
 
 		// Name of folder to export file to
@@ -27,25 +34,67 @@ class MeshExporter{
 		std::string file_prefix_ = "mesh_";
 	
 	public:
+		//pointer to the values of the level set function
 		Eigen::VectorXd plevel_set_;
-		Eigen::MatrixXd points_;
-		Eigen::MatrixXd vertices_;
-		Eigen::MatrixXi faces_;
+		
+		//vector which contains the numerator of the value x_average
+		//used by the level set function
 		std::vector<Eigen::Vector3d> x_avrg_num;
+		
+		//pointer to the list which constains the numerator of the value
+		//r_average used by the level set function
 		double* r_avrg_num;
+		
+		//pointer to the list of the denominator used by both x_average and 
+		//r_average (used by the level set function)
 		double* den;
 		
+		//matrix with the coordinate of the points in which the level set 
+		//function is computed. Every row represents the coordinate of a point
+		Eigen::MatrixXd points_;
+		
+		//matrix to store the vertices of the mesh (used by marching_cubes)
+		Eigen::MatrixXd vertices_;
+		
+		//matrix to store the faces of the mesh (used by marching_cubes)
+		Eigen::MatrixXi faces_;
+		
+		/**Constructor
+		 * Params:
+		 * - Grid is a pointer to a Mac3d grid
+		 * - particles is a pointer to a list of Particle
+		 * - n is the number of particles contained in particles
+		 */
 		MeshExporter(Mac3d* Grid, Particle* particles, const int n);
 		
+		/**Destructor
+		 */
 		~MeshExporter(){
 			num_particles_ = 0;
 			delete[] r_avrg_num;
 			delete[] den;
 		}
 		
+		/**Level set function:
+		 * computes the value of the level set function at every point in 
+		 * the points_ list. The value of the level set function is bigger 
+		 * than 0 if the point in which is evaluated lies out of the fluid 
+		 * and smaller than 0 else. At the point where the level set function 
+		 * is exactly 0 lies the surface of the fluid.
+		 */
 		void level_set();
+		
+		/**Level set function "easy version":
+		 * computes the value of the level set function at every point in 
+		 * the points_ list. The value of the level set function is 1+
+		 * if the point in which is evaluated lies out of the fluid 
+		 * and -1 else. At the point where the level set function 
+		 * is exactly 0 lies the surface of the fluid.
+		 */
 		void level_set_easy();
-
+		
+		/**Export the mesh in an obj file
+		 */
 		void export_mesh();
 };
 

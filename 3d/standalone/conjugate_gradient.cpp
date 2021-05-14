8d7475321e6be378f6e097bc4622c16626deff21
Felix Illes <felix@ethz.cethz.ch>
2021-05-14 20:20:00 +0200

    initial draft of CG algo

--- 3d/standalone/conjugate_gradient.cpp
+++ 3d/standalone/conjugate_gradient.cpp
@@ -7,19 +7,23 @@ int main() {
 	// build a test matrix A
 	constexpr int nnz = 20;
 	constexpr int n_rows = 7;
-	double values[nnz];
-	int col_idx[nnz];
-	int row_idx[n_rows];
+	unsigned max_steps = 100;
 
-	// build a rhs
+	cg::SparseMat A ( 20,7);
+    cg::SparseMat Pre ( 20,7);
+
+
+    // build a rhs
 	double rhs[n_rows];
 
 	// instaciate the solver
-	cg::ICConjugateGradientSolver solver;
+	cg::ICConjugateGradientSolver solver(Pre, max_steps);
 
 	// solve the sparse LSE
 	double solution[n_rows];
 
-	solver.solve(values, row_idx, col_idx, rhs, solution);
+	//solver.solve(A.values, A.row_idx, A.col_idx, rhs, solution);
+    solver.solve(&A, rhs, solution);
+
 
 }

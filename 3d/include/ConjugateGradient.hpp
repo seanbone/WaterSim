b17d264eeba512e56cd7a94a0893acb9f85809f9
Felix Illes <felix@ethz.cethz.ch>
2021-05-27 15:45:10 +0200

    added some peel loops fr alligned loads

--- 3d/include/ConjugateGradient.hpp
+++ 3d/include/ConjugateGradient.hpp
@@ -48,8 +48,13 @@ class ICConjugateGradientSolver {
 	// diagonal of preconditioner matrix
 	double *precon_diag;
 
+<<<<<<< HEAD
 	// diagonal of matrix A
 	double* A_diag;
+=======
+	    // variables for optimisation
+	    const double d_size_inv = 1/sizeof(double);
+>>>>>>> 1ae03e7 (added some peel loops fr alligned loads)
 
 	// current step and max steps
 	unsigned step, max_steps;
@@ -57,7 +62,13 @@ class ICConjugateGradientSolver {
 	// threshhold
 	const double thresh = 1e-9;
 
+<<<<<<< HEAD
 	// unsure where rho comes from
+=======
+        // not sure where to put this
+        //res[i] = a[i] * b + c[i]
+        inline void sca_product(const double* a, double b, const double* c,  const unsigned n, double *res);
+>>>>>>> 1ae03e7 (added some peel loops fr alligned loads)
 
 	public:
 	ICConjugateGradientSolver();
@@ -70,6 +81,7 @@ class ICConjugateGradientSolver {
 
 	// not sure where to put this
 
+<<<<<<< HEAD
 	static void Mat_mult(SparseMat *M, const double *a, double *res);
 
 	void solve(const double* rhs, double* p);
@@ -96,5 +108,146 @@ inline void ICConjugateGradientSolver::Mat_mult(SparseMat *M, const double *a, d
 	}
 }
 
+=======
+        void solve(const double* rhs, double* p);
+	};
+}
+
+inline double cg::ICConjugateGradientSolver::dot_product(const double *a, const double *b, const unsigned int n) {
+    double tmp = 0;
+    unsigned i = 0;
+    // we want 2 FMAs because of Skylake ports
+    // so we do 8 doubles per step
+    __m256d vec_a1, vec_a2, vec_b1, vec_b2, sol;
+    __m256d vec_por1 = _mm256_setzero_pd();
+    __m256d vec_por2 = _mm256_setzero_pd();
+
+    // peel loop for alligned loads for a ! b should be handled too.
+    auto peel = (unsigned long) a & 0x1f;
+    if (peel != 0){
+        peel = ( 32-peel)/sizeof(double);
+        for(; i < peel ;++i){
+            tmp += a[i]*b[i];
+        }
+    }
+
+    //! b should be unalligned loads?
+    for(; i < n-8; i +=8 ) {
+        vec_a1 = _mm256_load_pd(a+i);
+        vec_b1 = _mm256_load_pd(b+i);
+        vec_a2 = _mm256_load_pd(a+4+i);
+        vec_b2 = _mm256_load_pd(b+4+i);
+
+        // not sure if we get aliasing issues here
+        vec_por1 = _mm256_fmadd_pd(vec_a1,vec_b1,vec_por1);
+        vec_por2 = _mm256_fmadd_pd(vec_a2,vec_b2,vec_por2);
+    }
+    // reduce all 8 into 1
+    sol = _mm256_hadd_pd(vec_por1,vec_por2);
+    sol = _mm256_hadd_pd(sol,sol);
+    //??? not sure
+    tmp += sol[0] + sol[2];
+    for(; i < n; ++i ) {
+        tmp += a[i]*b[i];
+    }
+    return tmp;
+}
+/*
+inline void cg::ICConjugateGradientSolver::Mat_mult(SparseMat *M, const double *a, double *res) {
+    const unsigned v = M->v;
+    double tmp = 0;
+    // we assume first val of row dx is 0
+    // and last val is past the end val by convention
+    unsigned *curr_row_idx = M->row_idx;
+    unsigned *curr_col_idx = M->col_idx;
+
+    for(unsigned j = 0; j < v;){
+        tmp = 0;
+        while(j < *(curr_row_idx+1)){
+            tmp += M->values[j] * a[*curr_col_idx];
+            ++j;
+            ++curr_col_idx;
+        }
+        res[*curr_row_idx] = tmp;
+        ++curr_row_idx;
+    }
+}
+
+inline void cg::ICConjugateGradientSolver::sca_add_product(const double *a, const double b, const unsigned int n, double *res) {
+    for(unsigned i = 0; i < n; ++i ) {
+        res [i] += a[i] * b;
+    }
+}
+
+*/
+inline void cg::ICConjugateGradientSolver::sca_add_product(const double *a, const double b, const unsigned int n, double *res) {
+    unsigned i = 0;
+    // we want 2 FMAs because of Skylake ports
+    // so we do 8 doubles per step
+    __m256d vec_a1, vec_a2, vec_res, vec_res1, vec_res2, vec_res3;
+    __m256d vec_b1 = _mm256_set1_pd(b);
+    __m256d vec_b2 = _mm256_set1_pd(b);
+
+    // peel loop for alligned loads for a ! b should be handled too.
+    auto peel = (unsigned long) a & 0x1f;
+    if (peel != 0){
+        peel = ( 32-peel) *d_size_inv;
+        for(; i < peel ;++i){
+            res [i] += a[i] * b;
+        }
+    }
+    for(; i < n-8; i +=8 ) {
+        vec_a1      = _mm256_loadu_pd(a+i);
+        vec_a2      = _mm256_loadu_pd(a+i+4);
+        vec_res     = _mm256_loadu_pd(res+i);
+        vec_res1    = _mm256_loadu_pd(res+i+4);
+
+        // not sure if we get aliasing issues here
+        vec_res2 = _mm256_fmadd_pd(vec_a1,vec_b1,vec_res);
+        vec_res3 = _mm256_fmadd_pd(vec_a2,vec_b2,vec_res1);
+        _mm256_storeu_pd(res+i,vec_res2);
+        _mm256_storeu_pd(res+i+4,vec_res3);
+    }
+    for(; i < n; ++i ) {
+        res [i] += a[i] * b;
+    }
+}
+
+inline void cg::ICConjugateGradientSolver::sca_product(const double *a, double b, const double *c, const unsigned int n, double *res) {
+    unsigned i = 0;
+    // we want 2 FMAs because of Skylake ports
+    // so we do 8 doubles per step
+    __m256d vec_a1, vec_a2, vec_c1, vec_c2, vec_res, vec_res1;
+    __m256d vec_b1 = _mm256_set1_pd(b);
+    __m256d vec_b2 = _mm256_set1_pd(b);
+
+    // peel loop for alligned loads for a ! b should be handled too.
+    auto peel = (unsigned long) a & 0x1f;
+    if (peel != 0){
+        peel = ( 32-peel)/sizeof(double);
+        for(; i < peel ;++i){
+            res[i] = a[i] * b + c[i];
+        }
+    }
+
+    for (; i < n - 8; i += 8) {
+        vec_a1 = _mm256_load_pd(a + i);
+        vec_a2 = _mm256_load_pd(a + i + 4);
+        vec_c1 = _mm256_load_pd(c + i);
+        vec_c2 = _mm256_load_pd(c + i + 4);
+
+        // not sure if we get aliasing issues here
+        vec_res = _mm256_fmadd_pd(vec_a1, vec_b1, vec_c1);
+        vec_res1 = _mm256_fmadd_pd(vec_a2, vec_b2, vec_c2);
+        _mm256_storeu_pd(res + i, vec_res);
+        _mm256_storeu_pd(res + i + 4, vec_res1);
+    }
+    for(; i < n; ++i ) {
+        res[i] = a[i] * b + c[i];
+    }
+}
+
+
+>>>>>>> 1ae03e7 (added some peel loops fr alligned loads)
 
 #endif

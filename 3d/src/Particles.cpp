d08ec35807882bf61cee4d6404e8f397d87d84b3
Felix Illes <felix@ethz.cethz.ch>
2021-05-29 15:06:33 +0200

    removed lavalamp buremoved lavalamp bug

--- 3d/src/Particles.cpp
+++ 3d/src/Particles.cpp
@@ -5,20 +5,22 @@
 Particles::Particles(Particles::particleIdx_t nParticles, const Mac3d &macGrid)
 		: rcell_size_x_(1.0 / macGrid.get_cell_sizex()), rcell_size_y_(1.0 / macGrid.get_cell_sizey()),
 		  rcell_size_z_(1.0 / macGrid.get_cell_sizez()), num_particles_(nParticles) {
+
+	x =  (double *) malloc(nParticles * sizeof(double));
+	y =  (double *) malloc(nParticles * sizeof(double));
+	z =  (double *) malloc(nParticles * sizeof(double));
+	u =  (double *) malloc(nParticles * sizeof(double));
+	v =  (double *) malloc(nParticles * sizeof(double));
+	w =  (double *) malloc(nParticles * sizeof(double));
+
     /*
-	x =  (double *) calloc(nParticles, sizeof(double));
-	y =  (double *) calloc(nParticles, sizeof(double));
-	z =  (double *) calloc(nParticles, sizeof(double));
-	u =  (double *) calloc(nParticles, sizeof(double));
-	v =  (double *) calloc(nParticles, sizeof(double));
-	w =  (double *) calloc(nParticles, sizeof(double));
-	*/
     x =  new (std::align_val_t(32)) double [nParticles];
     y =  new (std::align_val_t(32)) double [nParticles];
     z =  new (std::align_val_t(32)) double [nParticles];
     u =  new (std::align_val_t(32)) double [nParticles];
     v =  new (std::align_val_t(32)) double [nParticles];
     w =  new (std::align_val_t(32)) double [nParticles];
+    */
 }
 
 Particles::~Particles() {
